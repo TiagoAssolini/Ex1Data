e9642e77b594219e4de684dfbfb150677779e57c
TiagoAssolini <tiagoassolini@gmail.com>
2024-05-23 20:32:27 -0300

    teste

--- Ex1Data.py
+++ Ex1Data.py
@@ -1,6 +1,6 @@
 
 #Creating list, tuple, set and dictionary
-list1 = ['banana', 'maça', 3, True, 1, 'banana']
+list1 = ['banana', 'maça', 2, True, 0, 'banana']
 tuple1 = ('banana', 'maça', 2, True, 1,'banana')
 set1 = {'banana', 'maça', 2, True, 1, 'banana'}
 dict1 = {'banana':'amarela', 'maça':'vermelha', 'quantidade':2, 'Status':True, 'Quantidade':1, 'banana':'amarela'}
